7230838527bf75c60791948a6eb9a6d05f0fdfd6
Michael Moore <michael.moore@carahsoft.com>
2024-09-03 17:10:16 -0400

    added console table class

---
+++ Carahsoft.Calliope/ColumnHeader.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carahsoft.Calliope
+{
+    public class ColumnHeader
+    {
+        string _formatString;
+        public ColumnHeader()
+        {
+            Alignment = ColumnAlignment.Left;
+        }
+
+        public int Width { get; set; }
+        public string Name { get; set; }
+
+        public ColumnAlignment Alignment { get; set; }
+        public int Rows { get; set; }
+
+        public string FormatString
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_formatString))
+                {
+                    _formatString = "{0," + Width * (Alignment == ColumnAlignment.Left ? -1 : 1) + "}";
+                }
+
+                return _formatString;
+            }
+        }
+    }
+}
---
+++ Carahsoft.Calliope/ConsoleTable.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carahsoft.Calliope
+{
+    public class ConsoleTable
+    {
+
+        DataTable tab;
+        List<ColumnHeader> cols;
+        public ConsoleTable(DataTable table)
+        {
+            tab = table;
+            SetHeaders();
+        }
+
+        private void SetHeaders()
+        {
+            cols = new List<ColumnHeader>();
+
+            foreach (DataColumn col in tab.Columns)
+            {
+                var colHeader = new ColumnHeader();
+
+                colHeader.Name = col.ColumnName;
+
+                string maxString = tab.AsEnumerable()
+                                        .Select(row => row[col].ToString())
+                                        .OrderByDescending(st => st.Length).FirstOrDefault();
+
+                if (maxString.Length > col.ColumnName.Length)
+                {
+                    colHeader.Width = maxString.Length+2;
+                }
+                else
+                    colHeader.Width = col.ColumnName.Length+2;
+
+                Debug.WriteLine(colHeader.Name + "\t" + colHeader.Width + "\t" + maxString);
+
+                cols.Add(colHeader);
+
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            StringBuilder spacer = new StringBuilder();
+
+
+            sb.Append(" | ");
+
+            spacer.Append(" +-");
+            foreach (var c in cols)
+            {
+                sb.Append(string.Format(c.FormatString, c.Name));
+                sb.Append(" | ");
+
+                spacer.Append(string.Empty.PadLeft(c.Width, '-'));
+                spacer.Append("-+-");
+            }
+            sb.Append('\n');
+            sb.Append(spacer.ToString());
+            sb.Append('\n');
+
+
+            foreach (DataRow dr in tab.Rows)
+            {
+                sb.Append(" | ");
+
+                foreach (var c in cols)
+                {
+
+                    string val = dr[c.Name].ToString();
+
+                    if(c.Alignment==ColumnAlignment.Center)
+                        val = val.PadRight(((c.Width - val.Length) / 2) + val.Length, ' ');
+
+                    sb.Append(string.Format(c.FormatString, val));
+
+                    sb.Append(" | ");
+                }
+                sb.Append('\n');
+            }
+
+            return sb.ToString();
+        }
+  
+    }
+
+ 
+
+ 
+}
