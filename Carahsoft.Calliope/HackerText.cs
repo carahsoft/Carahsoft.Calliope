05bbbf3bc7e0dc257cb8b8f6dc41bdb839ba0e7d
Michael H. Moore <mhmoore@Carahsoft.com>
2024-09-05 11:50:26 -0400

    added hacker text

---
+++ Carahsoft.Calliope/HackerText.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carahsoft.Calliope
+{
+    public class HackerWriter : TextWriter
+    {
+        TextWriter StandardOut;
+        public HackerWriter(TextWriter standardOut) 
+        {
+            StandardOut = standardOut;
+        }
+        public override void Write(char value)
+        {
+            StandardOut.Write(value);
+        }
+
+        public override void Write(string value)
+        {
+                int i = 0;
+                foreach (char c in value)
+                {
+                    i++;
+                    if ((char.IsLetterOrDigit(c) || c == ' ') && i % 5 == 0)
+                        Thread.Sleep(1);
+                    Write(c);
+                }
+        }
+
+        public override void WriteLine(string value)
+        {
+            Write(value);
+            Write("\n");
+        }
+
+        public override Encoding Encoding
+        {
+            get
+            {
+                return Encoding.UTF8;
+            }
+        }
+    }
+}
--- Carahsoft.Calliope/Table/ConsoleTable.cs
+++ Carahsoft.Calliope/Table/ConsoleTable.cs
@@ -1,24 +1,56 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.Design;
 using System.Data;
 using System.Data.Common;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.JavaScript;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Carahsoft.Calliope.Table
 {
-    public class ConsoleTable
+    public class ConsoleTable<T>
     {
 
         DataTable tab;
         List<ColumnHeader> cols;
+
         public ConsoleTable(DataTable table)
         {
             tab = table;
             SetHeaders();
         }
+
+        public ConsoleTable(IEnumerable<T> myList)
+        {
+            var testList = myList.ToList();
+
+            DataTable dt = new DataTable();
+
+            foreach (var o in testList[0].GetType().GetProperties())
+            {
+                dt.Columns.Add(o.Name, o.PropertyType);
+            }
+            
+            foreach (var obj in testList)
+            {
+                var alist = new List<object>();
+                foreach(var whatever in obj.GetType().GetProperties())
+                {
+                    alist.Add(whatever.GetValue(obj));
+                }
+               dt.Rows.Add(alist.ToArray());
+            }
+            
+            tab = dt;
+            SetHeaders();
+        }
+
 
         private void SetHeaders()
         {
@@ -36,13 +68,28 @@ namespace Carahsoft.Calliope.Table
 
                 if (maxString.Length > col.ColumnName.Length)
                 {
-                    colHeader.Width = maxString.Length + 2;
+                    colHeader.Width = maxString.Length ;
                 }
                 else
-                    colHeader.Width = col.ColumnName.Length + 2;
+                {
+                    colHeader.Width = col.ColumnName.Length ;
+                }
+
+
+                if (col.DataType == typeof(int))
+                {
+                    colHeader.Alignment = ColumnAlignment.Center;
+                }
+                else if (col.DataType == typeof(decimal))
+                {
+                    colHeader.Alignment = ColumnAlignment.Right;
+                }
+                else
+                {
+                    colHeader.Alignment = ColumnAlignment.Left;
+                }
 
                 cols.Add(colHeader);
-
             }
         }
 
@@ -56,6 +103,7 @@ namespace Carahsoft.Calliope.Table
             sb.Append(" | ");
 
             spacer.Append(" +-");
+            
             foreach (var c in cols)
             {
                 sb.Append(string.Format(c.FormatString, c.Name));
@@ -75,11 +123,10 @@ namespace Carahsoft.Calliope.Table
 
                 foreach (var c in cols)
                 {
-
                     string val = dr[c.Name].ToString();
 
                     if (c.Alignment == ColumnAlignment.Center)
-                        val = val.PadRight((c.Width - val.Length) / 2 + val.Length, ' ');
+                        val = val.PadRight((int)Math.Ceiling(((decimal)c.Width - val.Length) / 2 + val.Length), ' ');
 
                     sb.Append(string.Format(c.FormatString, val));
 
