1f0af8c9c4728a8dfb8b6fc1ae435fbb59f5811a
Michael Moore <michael.moore@carahsoft.com>
2024-09-23 10:01:48 -0400

    commented code again

--- Carahsoft.Calliope/Table/ColumnAlignment.cs
+++ Carahsoft.Calliope/Table/ColumnAlignment.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Carahsoft.Calliope.Table
 {
+    /// <summary>
+    /// Specifies horizontal alignmnet of data in table cell
+    /// </summary>
     public enum ColumnAlignment
     {
         Left,
