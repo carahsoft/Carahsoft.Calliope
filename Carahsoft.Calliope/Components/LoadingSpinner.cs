3deff2d93a25506e9057e9365027996e1cde5462
ngeorge <nathan.george@carahsoft.com>
2024-09-24 15:04:09 -0400

    add Loading Spinner component

---
+++ Carahsoft.Calliope/Components/LoadingSpinner.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carahsoft.Calliope.Components
+{
+    public class LoadingSpinner : ICalliopeProgram
+    {
+        private const string BRAILLE_LOADER = "⣽⣾⣷⣯⣟⡿⢿⣻";
+
+        private int _frame;
+        private bool _spinning;
+
+        public CalliopeCmd? Init()
+        {
+            return Spin();
+        }
+
+        public CalliopeCmd? Update(CalliopeMsg msg)
+        {
+            if (msg is SpinMsg && _spinning)
+            {
+                _frame++;
+                if (_frame >= BRAILLE_LOADER.Length)
+                {
+                    _frame = 0;
+                }
+                return Spin();
+            }
+
+            return null;
+        }
+
+        public string View()
+        {
+            if (_spinning)
+                return BRAILLE_LOADER[_frame].ToString();
+
+            return "";
+        }
+
+        public CalliopeCmd Spin()
+        {
+            _spinning = true;
+            return CalliopeCmd.Make(async () =>
+            {
+                await Task.Delay(100);
+                return new SpinMsg();
+            });
+        }
+
+        public void StopSpin()
+        {
+            _spinning = false;
+            _frame = 0;
+        }
+
+        private class SpinMsg : CalliopeMsg { }
+    }
+}
---
+++ Examples/LoadingSpinnerExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using Carahsoft.Calliope;
+using Carahsoft.Calliope.Components;
+
+await Calliope.NewProgram(new LoadingSpinnerExample()).RunAsync();
+
+public class LoadingSpinnerExample : ICalliopeProgram
+{
+    LoadingSpinner _spin = new();
+
+    public CalliopeCmd? Init()
+    {
+        return _spin.Init();
+    }
+
+    public CalliopeCmd? Update(CalliopeMsg msg)
+    {
+        if (msg is KeyPressMsg kpm)
+        {
+            if (kpm.Key == ConsoleKey.D1)
+            {
+                return _spin.Spin();
+            }
+            if (kpm.Key == ConsoleKey.D2)
+            {
+                _spin.StopSpin();
+                return null;
+            }
+        }
+
+        return _spin.Update(msg);
+    }
+
+    public string View()
+    {
+        return $"""
+            This is a spinner! Press 1 to start the spinner and 2 to stop the spinner.
+
+            {_spin.View()}
+            """;
+    }
+}
+
